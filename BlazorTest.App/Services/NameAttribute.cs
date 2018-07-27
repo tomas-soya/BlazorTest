04295d3495cd57306bc7158a3a61de07a8b64b9e
tomas-soya <tomas947327312@qq.com>
2018-07-27 23:10:00 +0800

    添加文件

---
+++ BlazorTest.App/Services/NameAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTest.App.Services
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public sealed class NameAttribute : Attribute
+    {
+        private readonly string _name;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public NameAttribute(string name)
+        {
+            _name = name;
+        }
+    }
+}
--- BlazorTest.App/Services/WeatherForecast.cs
+++ BlazorTest.App/Services/WeatherForecast.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Diagnostics;
 
 namespace BlazorTest.App.Services
 {
     public class WeatherForecast
     {
+        [Name("日期")]
         public DateTime Date { get; set; }
+        [Name("摄氏度")]
         public int TemperatureC { get; set; }
+        [Name("华氏度")]
         public int TemperatureF { get; set; }
+        [Name("描述")]
         public string Summary { get; set; }
     }
 }
